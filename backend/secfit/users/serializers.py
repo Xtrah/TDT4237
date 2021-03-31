4cfee24240dfb0c5c23b05bdf6f8209ff6ec549f
Livia Emilie Valenti Stokke <livia.e.v.stokke@ntnu.no>
2021-03-31 14:37:51 +0200

    remove: unneccesary code

--- backend/secfit/users/serializers.py
+++ backend/secfit/users/serializers.py
@@ -104,7 +104,7 @@ class CustomTokenCreateSerializer(TokenCreateSerializer):
             self.user = User.objects.filter(**params).first()
             if self.user and not self.user.check_password(password):
                 self.fail("invalid_credentials")
-        if self.user: # and self.user.is_active: 
+        if self.user: 
             return attrs
         self.fail("invalid_credentials")
 
--- frontend/www/scripts/register.js
+++ frontend/www/scripts/register.js
@@ -10,22 +10,6 @@ async function createNewUser(event) {
       document.body.prepend(alert);
     
     } else {
-      // Hide login redirect
-      /*let body = {username: formData.get("username"), password: formData.get("password")};
-      response = await sendRequest("POST", `${HOST}/api/v1/token/login/`, body);
-      if (response.ok) {
-          let data = await response.json();
-          setCookie("access", data.access, 86400, "/");
-          setCookie("refresh", data.refresh, 86400, "/");
-          sessionStorage.setItem("username", formData.get("username"));
-      } else {
-        console.log("CAN'T GET JWT TOKEN ON REGISTRATION");
-        let data = await response.json();
-        let alert = createAlert("Registration could not complete. Try again!", data);
-        document.body.prepend(alert);
-      }
-      form.reset();
-      window.location.replace("workouts.html");*/
       let data = await response.json();
       let email = formData.get("email");
       let alert = createAlert("Registration completed! Verify your account by going to your email!", email.toString);
