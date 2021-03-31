bfa2b9e7987139ac47384e2c908164ccb5b37c1c
Xtrah <xtrah@live.com>
2021-03-31 18:51:41 +0200

    2FA finished

--- frontend/www/scripts/2fa.js
+++ frontend/www/scripts/2fa.js
@@ -1,35 +1,53 @@
-// send(host + "/api/totp/create/", {
-//     method: "POST",
-//     body: JSON.stringify(data)
-// }).then(res => {
-//     console.log("Request complete! response:", res);
-//     new QRCode(document.getElementById("qrcode"), "res");
-// });
 
 async function is2faEnabled() {
-    let user = await getCurrentUser();
-    let enabled = false;
+  let user = await getCurrentUser();
+  document.getElementById("inputUsername").value = user.username;
 
-    if (user.is_two_factor_enabled) {
-        document.querySelector("#enabled2fa").classList.remove("hide");
-        console.log("User is 2FA enabled :D")
+  if (user.is_two_factor_enabled) {
+    document.getElementById("enabled2fa").classList.remove("hide");
+  } else {
+    document.getElementById("activate2fa").classList.remove("hide");
 
+    let host = `${HOST}`
+    response = await sendRequest("GET", host + "/api/totp/create/");
+    if (!response.ok) {
+      let alert = createAlert("Something is off. Try again.", {});
+      document.body.prepend(alert);
     } else {
-        document.querySelector("#activate2fa").classList.remove("hide");
-        console.log("User is not 2FA enabled :(")
-
-        let data = getCookieValue("access");
-        let host = `${HOST}`
-        response = await sendRequest("GET", host + "/api/totp/create/");
-        let res = await response.json();
-        console.log(res)
-        new QRCode(document.getElementById("qrcode"), {
-            text: res,
-            // TODO: Styling
-        });
-        // Create POST request from input field and button
-        // If POST returns true, set user.is_two_factor_enabled = true
+      let res = await response.json();
+      new QRCode(document.getElementById("qrcode"), {
+        text: res,
+        // TODO: Styling
+      });
     }
+
+  }
 }
 
-is2faEnabled();
+window.addEventListener('load', () => {
+  is2faEnabled();
+});
+
+async function enable2FA(event) {
+  let user = await getCurrentUser();
+  let form = document.getElementById("2fa-form");
+  let formData = new FormData(form);
+  let inputCode = formData.get("inputCode")
+  let body = {
+    "username": user.username,
+    "password": formData.get("password")
+  };
+  let response = await sendRequest("POST", `${HOST}/api/totp/login/${inputCode}/`, body);
+
+  if (!response.ok) {
+    let alert = createAlert("Something is off. Try again.", {});
+    document.body.prepend(alert);
+  } else {
+    let alert = createAlert("Success! You are now authenticated for 2FA-only pages!", {});
+    document.body.prepend(alert);
+  }
+  form.reset();
+  document.getElementById("inputUsername").value = user.username;
+}
+
+document.getElementById("btn-activate-2fa").addEventListener("click", async () => await enable2FA());
